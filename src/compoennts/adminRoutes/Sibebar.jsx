3ebd0b8a7175851d583292eee25a05a7a6eef115
Afhaam k <afhaamcoc@gmail.com>
2021-12-18 10:00:48 +0530

    dahboard minir chnage

--- src/compoennts/adminRoutes/Sibebar.jsx
+++ src/compoennts/adminRoutes/Sibebar.jsx
@@ -27,7 +27,7 @@ function Sibebar() {
 
   return (
     <div>
-      <div className="sidebar">
+      <div git className="sidebar">
         <div className="logo-details">
           <i className="bx bxl-c-plus-plus"></i>
           <span className="logo_name">CK SHOP ADMIN</span>
