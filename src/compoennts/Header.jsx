244201f1c2bd1ebfe3312edca0e8922ed8570983
Afhaam k <afhaamcoc@gmail.com>
2021-12-18 10:36:10 +0530

    asas

--- src/compoennts/Header.jsx
+++ src/compoennts/Header.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import React from 'react'
 
 function Header({value}) {
-    const [active,setActive]=useState(false)
 
     // function toggleHandler(){
     //     setActive(!active)
@@ -13,25 +11,7 @@ function Header({value}) {
 
     return (
         <div >
-            <Navbar bg="light" expand="lg">
-            <Container>
-              <Navbar.Brand href="#home">{value}</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
-                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-                  </Navbar>
+        
         </div>
                 )
 }
